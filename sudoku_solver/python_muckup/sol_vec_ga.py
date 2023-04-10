f976b11a1b151b444d84bddaaa693198101addc8
bemmal <el18el@leeds.ac.uk>
2023-04-10 17:00:53 +0100

    GA Optimiser without Errors. Still Non Functional

--- sudoku_solver/python_muckup/sol_vec_ga.py
+++ sudoku_solver/python_muckup/sol_vec_ga.py
@@ -1,71 +1,128 @@
-from chn_solver import CHNSudokuSolver
+import numpy as np
+import random
 
+from chn_solver import CHNSudokuSolver
 class GASudokuSolver(CHNSudokuSolver):
-    def __init__(self, weights=None, max_iter=10000, pop_size=100, elite_size=20, mutation_rate=0.05):
-        super().__init__(weights)
-        self.max_iter = max_iter
-        self.pop_size = pop_size
-        self.elite_size = elite_size
-        self.mutation_rate = mutation_rate
-    
+    def __init__(self, solver=None):
+        super().__init__()
+        self.num_neurons = 729
+        self.weights = np.zeros((self.num_neurons, self.num_neurons))
+        self.pop_size = 100
+        self.mutation_rate = 0.01
+        self.elite_size = 10
+        self.generations = 50
+
     def solve_sudoku(self, grid):
-        # Initialize the population
-        population = [self.sudoku_to_vector(self.generate_random_sudoku(grid)) for i in range(self.pop_size)]
+        # Convert the Sudoku grid to a flat list
+        initial_solution = self.sudoku_to_vector(grid)
         
-        # Loop until the maximum number of iterations is reached
-        for i in range(self.max_iter):
-            # Calculate the fitness of each solution vector
-            fitness_scores = [self.calculate_fitness(solution) for solution in population]
-            
-            # Identify the elite solutions
-            elite_indices = np.argsort(fitness_scores)[::-1][:self.elite_size]
-            elite_population = [population[index] for index in elite_indices]
-            
-            # Create the next generation of solutions using crossover and mutation
-            children = []
-            while len(children) < self.pop_size - self.elite_size:
-                parent1, parent2 = np.random.choice(elite_population, size=2, replace=False)
-                child = self.crossover(parent1, parent2)
-                child = self.mutate(child)
-                children.append(child)
-            
-            # Combine the elite solutions with the children to create the next population
-            population = elite_population + children
-            
-            # Check if a valid solution has been found
-            for solution in population:
-                if self.is_valid_solution(solution):
-                    return self.vector_to_sudoku(solution)
-        
-        # If no valid solution is found, return None
-        return None
-    
-    def crossover(self, parent1, parent2):
-        # Perform uniform crossover
-        child = np.zeros_like(parent1)
-        for i in range(len(parent1)):
-            if np.random.rand() < 0.5:
-                child[i] = parent1[i]
-            else:
-                child[i] = parent2[i]
-        return child
-    
-    def mutate(self, solution):
-        # Perform bit-flip mutation
-        for i in range(len(solution)):
-            if np.random.rand() < self.mutation_rate:
-                solution[i] = 1 - solution[i]
-        return solution
+        # Create the initial population
+        population = self.create_population(initial_solution)
+
+        for i in range(self.generations):
+            # Rank the population
+            ranked_pop = self.rank_population(population)
+
+            # Select the parents for the next generation
+            selected_pop = self.selection(ranked_pop)
+
+            # Create the next generation
+            next_gen_pop = self.create_next_generation(selected_pop)
+
+            # Add the elite individuals to the next generation
+            next_gen_pop[0:self.elite_size] = ranked_pop[0:self.elite_size]
+
+            # Set the population for the next generation
+            population = next_gen_pop
+
+        # Return the solution with the best fitness score
+        return self.vector_to_sudoku(ranked_pop[0][0])
+
+    def create_population(self, solution):
+        # Create a list of populations with random values
+        population = []
+        for i in range(self.pop_size):
+            new_individual = solution[:]
+            random.shuffle(new_individual)
+            population.append(new_individual)
+        return population
     
     def calculate_fitness(self, solution):
-        # Calculate the energy of the solution vector
-        energy = np.dot(solution, np.dot(self.weights, solution)) / -2.0
-        
-        # Convert the solution vector to a Sudoku grid
-        grid = self.vector_to_sudoku(solution)
-        
-        # Calculate the number of conflicts in the Sudoku grid
-        conflicts = self.count_conflicts(grid)
-        
-        # Calculate the fitness score as the inverse of the energy plus the number of conflicts
-        return 1.0 / (energy + conflicts + 1)
+        """
+        Calculates the fitness of a given Sudoku solution, which is the sum of the absolute values
+        of the differences between each row, column, and 3x3 subgrid and the set {1, 2, ..., 9}.
+        """
+        fitness = 0
+
+        # Calculate the fitness for each row
+        for i in range(9):
+            row_sum = sum(solution[i * 9:(i + 1) * 9])
+            fitness += abs(row_sum - 45)
+
+        # Calculate the fitness for each column
+        for j in range(9):
+            col_sum = sum(solution[j::9])
+            fitness += abs(col_sum - 45)
+
+        # Calculate the fitness for each 3x3 subgrid
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                subgrid_sum = sum(solution[(i + x) * 9 + j:(i + x) * 9 + j + 3] for x in range(3))
+                fitness += abs(subgrid_sum - 45)
+
+        return fitness
+
+    def rank_population(self, population):
+        # Rank the population by fitness score
+        ranked_pop = []
+        for individual in population:
+            fitness_score = self.fitness(individual)
+            ranked_pop.append((individual, fitness_score))
+        ranked_pop.sort(key=lambda x: x[1], reverse=True)
+        return ranked_pop
+
+    def selection(self, ranked_pop):
+        # Select the parents for the next generation
+        selected_pop = []
+        total_fitness = sum([x[1] for x in ranked_pop])
+        for i in range(self.pop_size):
+            rand_num = random.uniform(0, total_fitness)
+            running_sum = 0
+            for j in range(len(ranked_pop)):
+                running_sum += ranked_pop[j][1]
+                if running_sum >= rand_num:
+                    selected_pop.append(ranked_pop[j][0])
+                    break
+        return selected_pop
+
+    def create_next_generation(self, selected_pop):
+        # Create the next generation
+        next_gen_pop = []
+        for i in range(self.pop_size):
+            parent_1 = random.choice(selected_pop)
+            parent_2 = random.choice(selected_pop)
+            child = self.crossover(parent_1, parent_2)
+            next_gen_pop.append(child)
+        return next_gen_pop
+
+    def crossover(self, parent_1, parent_2):
+        # Perform uniform crossover
+        child = []
+        for i in range(len(parent_1)):
+            if random.random() < 0.5:
+                child.append(parent_1[i])
+            else:
+                child.append(parent_2[i])
+        return child
+
+    def mutation(self, individual):
+        # Perform random mutation
+        for i in range(len(individual)):
+            if random.random() < self.mutation_rate:
+                individual[i] = random.randint(1, 9)
+        return individual
+
+    def fitness(self, individual):
+        # Calculate the fitness score
+        solution = self.vector_to_sudoku(individual)
+        return self.calculate_fitness
