1d6524edac085e78abf5ff974c278293f71517c0
Kaif-Kutchwala <kaifkutchwala@gmail.com>
2023-04-16 17:27:21 +0100

    Modify recall method to replicate asynchronous updates and return prediction label

--- handwriting_recognition/hopfield.py
+++ handwriting_recognition/hopfield.py
@@ -11,33 +11,36 @@ class HopfieldNetwork:
         self.weights = np.zeros((self.n, self.n))
         self.order = np.arange(0, self.n)
 
-    def update(self, input_pattern):
-        activation = self.get_activation(input_pattern)
-        return np.sign(activation)
-    
-    def get_activation(self, input_pattern):
-        return np.dot(self.weight_matrix, input_pattern)
 
-    def train(self, patterns):
-        num_patterns = patterns.shape[0]
-        for p in range(num_patterns):
-            pattern = patterns[p, :]
-            for i in range(self.neuron_count):
-                for j in range(self.neuron_count):
-                    if i != j:
-                        self.weight_matrix[i, j] += pattern[i] * pattern[j]
-        self.weight_matrix //= self.neuron_count
-
-    def recall(self, partial_pattern, iterations=10):
-        """
-        Recall a stored pattern closest to the input pattern.
-        """
-        state = np.copy(partial_pattern)
+    def recall(self, partial_pattern, iterations):
         for _ in range(iterations):
-            for i in range(self.neuron_count):
-                weights = self.weight_matrix[i, :]
+            np.random.shuffle(self.order)
+            state = np.copy(partial_pattern)
+            no_change_count = 0
+            for i in self.order:
+                weights = self.weights[i, :]
                 state[i] = np.sign(np.dot(weights, state))
             if np.array_equal(state, partial_pattern):
+                no_change_count += 1
+                if no_change_count > 3:
+                    break
+        return state, self.get_result_label(state)
+
+    def get_result_label(self, pattern):
+        file = open("mnist_digits_threshold.json", "r")
+        dataset = json.loads(file.read())["data"]
+        file.close()
+
+        closest_digit = 0
+        lowest_hamming_distance = len(pattern)
+        for label, digit in enumerate(dataset):
+            hamming_distance = self.get_hamming_distance(pattern, digit)
+            if hamming_distance < lowest_hamming_distance:
+                closest_digit = label
+                lowest_hamming_distance = hamming_distance
+
+        return closest_digit
+
     def get_hamming_distance(self, a, b):
         matrix1 = np.array(a).ravel()
         matrix2 = np.array(b).ravel()
