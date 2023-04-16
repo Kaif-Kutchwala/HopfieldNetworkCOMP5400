acbcc04438400ec90a9d22363e8e9330767afa6d
Kaif-Kutchwala <kaifkutchwala@gmail.com>
2023-04-16 17:28:06 +0100

    Add support for training with hebb, pseudoinverse and storkey rules

--- handwriting_recognition/hopfield.py
+++ handwriting_recognition/hopfield.py
@@ -1,5 +1,7 @@
 import numpy as np
 import json
+from itertools import product
+
 
 class HopfieldNetwork:
     def __init__(self, neuron_count):
@@ -11,6 +13,42 @@ class HopfieldNetwork:
         self.weights = np.zeros((self.n, self.n))
         self.order = np.arange(0, self.n)
 
+    def train(self, patterns, learning="pi"):
+        if learning == "pi":
+            # Pseudo Inverse
+            num_of_patterns = len(patterns)
+            c = np.tensordot(patterns, patterns, axes=(
+                (1), (1))) / num_of_patterns
+            cinv = np.linalg.inv(c)
+            for k, l in product(range(num_of_patterns), range(num_of_patterns)):
+                self.weights = self.weights + \
+                    cinv[k, l] * patterns[k] * patterns[l].reshape((-1, 1))
+            self.weights = self.weights / num_of_patterns
+        elif learning == "hebb":
+            for pattern in patterns:
+                self.weights += np.outer(pattern, pattern)
+            np.fill_diagonal(self.weights, 0)
+        elif learning == "storkey":
+            # Iterate through each pattern mu
+            for mu in range(len(patterns)):
+                print("Currently on Pattern", mu + 1, "of", len(patterns))
+                # Set each weight between neuron i and j
+                for i in range(self.n):
+                    for j in range((i - self.n) % self.n):
+                        # Calculate local field
+                        local_field = 0
+                        for k in range(self.n):
+                            if (k != i & k != j):
+                                if not np.isnan(self.weights[i][k]):
+                                    local_field += self.weights[i][k] * \
+                                        patterns[mu][k]
+                        # Increment weight for the pattern
+                        self.weights[i][j] += (1/self.n) * ((patterns[mu][i] * patterns[mu][j]) -
+                                                            ((1/self.n) * patterns[mu][i] * local_field) -
+                                                            ((1/self.n) * local_field * patterns[mu][j]))
+            np.fill_diagonal(self.weights, 0)
+        else:
+            pass
 
     def recall(self, partial_pattern, iterations):
         for _ in range(iterations):
@@ -45,5 +83,6 @@ class HopfieldNetwork:
         matrix1 = np.array(a).ravel()
         matrix2 = np.array(b).ravel()
 
-        hamming_distance = sum([1 if x != y else 0 for x,y in zip(matrix1,matrix2)])
+        hamming_distance = sum(
+            [1 if x != y else 0 for x, y in zip(matrix1, matrix2)])
         return hamming_distance
