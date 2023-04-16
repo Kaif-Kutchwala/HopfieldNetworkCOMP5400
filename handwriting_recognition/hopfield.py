0c85133c9a7057a6131a7de2b3f28b8bed392a89
Kaif-Kutchwala <kaifkutchwala@gmail.com>
2023-04-16 17:25:56 +0100

    Add method to calculate hamming distance in hopfield.py

--- handwriting_recognition/hopfield.py
+++ handwriting_recognition/hopfield.py
@@ -1,14 +1,15 @@
 import numpy as np
-import pygame
+import json
 
 class HopfieldNetwork:
     def __init__(self, neuron_count):
         # AKA 'N'
-        self.neuron_count = neuron_count
+        self.n = neuron_count
         # All neurons start with -1 as state
-        self.neuron_states = -1 * np.ones(self.neuron_count)
+        self.neuron_states = -1 * np.ones(self.n)
         # Weight matrix is initialised to be of size N*N with all weights set to 0
-        self.weight_matrix = np.zeros((neuron_count, neuron_count))
+        self.weights = np.zeros((self.n, self.n))
+        self.order = np.arange(0, self.n)
 
     def update(self, input_pattern):
         activation = self.get_activation(input_pattern)
@@ -37,17 +38,9 @@ class HopfieldNetwork:
                 weights = self.weight_matrix[i, :]
                 state[i] = np.sign(np.dot(weights, state))
             if np.array_equal(state, partial_pattern):
-                break
-        return state
-    
-    # def construct_hebb_matrix(xi):
-    #     n = xi.shape[0]
-    #     if len(xi.shape) == 1:
-    #         w = np.outer(xi, xi) / n  # p = 1
-    #     elif len(xi.shape) == 2:
-    #         w = np.einsum("ik,jk", xi, xi) / n  # p > 1
-    #     else:
-    #         raise ValueError("Unexpected shape of input pattern xi: {}".format(xi.shape))
-    #     np.fill_diagonal(w, 0)  # set diagonal elements to zero
-    #     return w
-        
+    def get_hamming_distance(self, a, b):
+        matrix1 = np.array(a).ravel()
+        matrix2 = np.array(b).ravel()
+
+        hamming_distance = sum([1 if x != y else 0 for x,y in zip(matrix1,matrix2)])
+        return hamming_distance
